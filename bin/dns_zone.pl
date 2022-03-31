111a5775fe829608f8fd5f523ffc2b5a8118c418
Colin <the-imt@users.noreply.github.com>
2022-03-31 11:31:13 -0400

    Update dns_zone.pl
    Added processing for AD zones

--- bin/dns_zone.pl
+++ bin/dns_zone.pl
@@ -3,18 +3,30 @@
 use lib './lib';
 
 use dns_grammar;
+use dns_grammar_AD;
 use dns_data;
 
 
-sub MAIN($source, $outfile)  {
+sub MAIN($source, $outfile, $type="def")  {
 
 	my $OutData = DNS_data.new();
-	for $source.IO.lines -> $line  {
-		next if $line ~~ /';'/;
-		my $DNS_parsed = DNS_grammar.parse($line, actions => DNS_captures.new);
-		next unless $DNS_parsed.defined();
-		my $GetIt = $DNS_parsed.made;
-		$OutData.ingest($GetIt<line>);
+	if $type eq "ad"  {
+		for $source.IO.lines -> $line  {
+			next if $line ~~ /';'/;
+			my $DNS_parsed = DNS_grammar_AD.parse($line, actions => DNS_captures.new);
+			next unless $DNS_parsed.defined();
+			my $GetIt = $DNS_parsed.made;
+			$OutData.ingest($GetIt<line>);
+		}	
+		
+	} else  {
+		for $source.IO.lines -> $line  {
+			next if $line ~~ /';'/;
+			my $DNS_parsed = DNS_grammar.parse($line, actions => DNS_captures.new);
+			next unless $DNS_parsed.defined();
+			my $GetIt = $DNS_parsed.made;
+			$OutData.ingest($GetIt<line>);
+		}
 	}
 	$OutData.csv_out($outfile);
 }
